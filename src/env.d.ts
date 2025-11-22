225fa38114fa24d52bb66fe8dc08b1c3bb66c8bb
Jerry Bruwes <jbruwes@icloud.com>
2025-11-22 18:32:13 +0200

    feat: add markdown-it plugins type declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -14,3 +14,11 @@ declare module "jsonfeed-to-rss";
 declare module "eslint-plugin-vue-pug" {
   export const configs: Record<string, unknown>;
 }
+declare module "@datatraccorporation/markdown-it-mermaid";
+declare module "markdown-it-abbr";
+declare module "markdown-it-deflist";
+declare module "markdown-it-ins";
+declare module "markdown-it-mark";
+declare module "markdown-it-sub";
+declare module "markdown-it-sup";
+declare module "markdown-it-task-lists";
