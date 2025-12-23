c54ef71c77c19452efd2e21e3f8528c35c51a3b8
Jerry Bruwes <jbruwes@icloud.com>
2025-12-23 16:16:28 +0200

    refactor: update quotes style in Pinia store configuration

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,12 @@
-import { defineStore } from '#q-app/wrappers'
-import { createPinia } from 'pinia'
+import { defineStore } from "#q-app/wrappers";
+import { createPinia } from "pinia";
 
 /*
  * When adding new properties to stores, you should also
  * extend the `PiniaCustomProperties` interface.
  * @see https://pinia.vuejs.org/core-concepts/plugins.html#typing-new-store-properties
  */
-declare module 'pinia' {
+declare module "pinia" {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface PiniaCustomProperties {
     // add your custom properties here, if any
@@ -23,10 +23,10 @@ declare module 'pinia' {
  */
 
 export default defineStore((/* { ssrContext } */) => {
-  const pinia = createPinia()
+  const pinia = createPinia();
 
   // You can add Pinia plugins here
   // pinia.use(SomePiniaPlugin)
 
-  return pinia
-})
+  return pinia;
+});
