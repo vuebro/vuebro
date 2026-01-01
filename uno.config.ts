c07cc2115b0cca9e0f06e850a41fee31ff9f0b99
Jerry Bruwes <jbruwes@icloud.com>
2026-01-01 15:05:36 +0200

    refactor: add type assertion for uno presets

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,10 @@
+import type { PresetOrFactoryAwaitable } from "unocss";
+
 import config from "@vuebro/configs/uno";
 import presets from "@vuebro/configs/uno/presets";
 import { defineConfig } from "unocss";
 
-export default defineConfig({ presets: presets(), ...config });
+export default defineConfig({
+  presets: presets() as PresetOrFactoryAwaitable[],
+  ...config,
+});
